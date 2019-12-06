bf4d51736fe0285115d6c9314bb2293e2552361c
George Zhao <george.zhao@siteminder.com>
2019-12-06 11:57:48 +1100

    Make type order same as parameters order

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 type Scalar = Exclude<any, object>
 
-declare function createError<E extends Error, T extends object, C extends Scalar>(error: E, code: C, props?: T): E & T & { code: C }
+declare function createError<E extends Error, C extends Scalar, T extends object>(error: E, code: C, props?: T): E & T & { code: C }
 declare function createError<E extends Error, T extends object>(error: E, props?: T): E & T
 
 declare namespace createError {
