590f81447853032bc6792b2c1ab0dd02bf57f5bb
achingbrain <alex@achingbrain.net>
2020-06-10 20:09:20 +0100

    fix: handle errors that throw on code assignment
    `DOMException`s can have a non-enumerable `.code` property that
    throws if you try to assign it and does not have a property descriptor.
    
    Take the nuclear option here and swallow thrown exceptions when we try
    to assign `.code` and any extra props.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
 
-function maybeDefineProperty(obj, name, value) {
-    var descriptor;
-
-    if (Object.isFrozen(obj)) {
-        return;
-    }
-
-    descriptor = Object.getOwnPropertyDescriptor(obj, name);
-
-    if (descriptor && !descriptor.writable) {
-        return;
-    }
-
-    obj[name] = value;
-}
-
 function createError(err, code, props) {
     var key;
 
@@ -26,12 +10,16 @@ function createError(err, code, props) {
     if (typeof code === 'object') {
         props = code;
     } else if (code != null) {
-        maybeDefineProperty(err, 'code', code);
+        try {
+            err.code = code
+        } catch (err) { }
     }
 
     if (props) {
         for (key in props) {
-            maybeDefineProperty(err, key, props[key]);
+            try {
+                err[key] = props[key]
+            } catch (err) { }
         }
     }
 
