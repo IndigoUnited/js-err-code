5d66a528b31ba66623d47f099e2f0de01ec07db6
achingbrain <alex@achingbrain.net>
2020-06-10 15:22:28 +0100

    chore: update umd build

--- index.umd.js
+++ index.umd.js
@@ -1,6 +1,22 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.errCode = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
+function maybeDefineProperty(obj, name, value) {
+    var descriptor;
+
+    if (Object.isFrozen(obj)) {
+        return;
+    }
+
+    descriptor = Object.getOwnPropertyDescriptor(obj, name);
+
+    if (descriptor && !descriptor.writable) {
+        return;
+    }
+
+    obj[name] = value;
+}
+
 function createError(err, code, props) {
     var key;
 
@@ -11,12 +27,12 @@ function createError(err, code, props) {
     if (typeof code === 'object') {
         props = code;
     } else if (code != null) {
-        err.code = code;
+        maybeDefineProperty(err, 'code', code);
     }
 
     if (props) {
         for (key in props) {
-            err[key] = props[key];
+            maybeDefineProperty(err, key, props[key]);
         }
     }
 
